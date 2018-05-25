92a0e04736f965c387e9247012fd8d7888287121
Piotr Dutko <p.dutko@webdudi.pl>
2018-05-25 23:06:54 +0200

    Update mbank-before-lifting.js
    Fixed position of "Oceń nas" button

--- mbank-before-lifting.js
+++ mbank-before-lifting.js
@@ -12,3 +12,8 @@ var elementsArray = [].slice.call(document.getElementsByClassName("lifting"));
 for (var i = 0; i < elementsArray.length; i++) {
     elementsArray[i].classList.remove("lifting");
 }
+
+setTimeout(function(){
+    document.getElementById('feedback-survey').getElementsByClassName('trigger-label')[0].style.top = "40px";
+    document.getElementById('feedback-survey').getElementsByClassName('trigger-label')[0].style.right = "16px";
+}, 0);
